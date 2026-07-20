aa91328303e8e486b4173c195bc9dbc7faed4e18
roepckie-source <roepckie@gmail.com>
2026-07-20 17:37:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,49 @@
+import os
 
+
+# ==============================
+# DISCORD
+# ==============================
+
+DISCORD_WEBHOOK = os.environ.get(
+    "DISCORD_WEBHOOK"
+)
+
+
+# ==============================
+# X / TWITTER
+# ==============================
+
+API_KEY = os.environ.get(
+    "API_KEY"
+)
+
+API_SECRET = os.environ.get(
+    "API_SECRET"
+)
+
+ACCESS_TOKEN = os.environ.get(
+    "ACCESS_TOKEN"
+)
+
+ACCESS_TOKEN_SECRET = os.environ.get(
+    "ACCESS_TOKEN_SECRET"
+)
+
+
+# ==============================
+# COINMARKETCAP (später)
+# ==============================
+
+CMC_API_KEY = os.environ.get(
+    "CMC_API_KEY"
+)
+
+
+# ==============================
+# EINSTELLUNGEN
+# ==============================
+
+BOT_NAME = "DeFiChain Daily Bot"
+
+VERSION = "2.0"
