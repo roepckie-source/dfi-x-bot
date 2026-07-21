436bd548faa69703a39596b619d8d014d244a220
roepckie-source <roepckie@gmail.com>
2026-07-21 16:23:26 +0200

    Update tokenomics.py

--- modules/tokenomics.py
+++ modules/tokenomics.py
@@ -1,1 +1,134 @@
+# ==============================
+# DeFiChain Daily Intelligence v4
+# Tokenomics Module
+# ==============================
 
+
+def get_tokenomics_data():
+
+
+    # Aktuelle Werte
+    # später ersetzen wir diese
+    # durch echte On-Chain Daten
+
+
+    burned = {
+
+        "address": 158909764.56,
+
+        "fee": 993026.96,
+
+        "auction": 3538007.76,
+
+        "payback": 61705058.17
+
+    }
+
+
+
+    total_burn = (
+
+        burned["address"]
+
+        + burned["fee"]
+
+        + burned["auction"]
+
+        + burned["payback"]
+
+    )
+
+
+
+    emission = 98815760.99
+
+
+
+    net_change = emission - total_burn
+
+
+
+    if net_change < 0:
+
+
+        status = "🟢 Deflationär"
+
+
+
+    else:
+
+
+        status = "🔴 Inflationär"
+
+
+
+
+    # einfacher Score
+    # später erweitern
+
+
+    if net_change < 0:
+
+
+        score = 80
+
+
+    else:
+
+
+        score = 40
+
+
+
+
+    return {
+
+
+        "burn": {
+
+
+            "address":
+            burned["address"],
+
+
+            "fee":
+            burned["fee"],
+
+
+            "auction":
+            burned["auction"],
+
+
+            "payback":
+            burned["payback"],
+
+
+            "total":
+            total_burn
+
+        },
+
+
+        "emission":
+
+        emission,
+
+
+
+        "net_change":
+
+        net_change,
+
+
+
+        "status":
+
+        status,
+
+
+
+        "score":
+
+        score
+
+    }
