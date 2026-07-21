5cfa611c8efe0b8db86268343f148e8cffb7918e
roepckie-source <roepckie@gmail.com>
2026-07-21 16:20:42 +0200

    Update language_manager.py

--- language_manager.py
+++ language_manager.py
@@ -1,1 +1,20 @@
+from datetime import datetime
 
+
+def get_daily_language():
+
+    languages = [
+        "en",
+        "de",
+        "es",
+        "zh",
+        "fr",
+        "hi",
+        "en"
+    ]
+
+
+    day = datetime.now().weekday()
+
+
+    return languages[day]
