844b5bc5bd431fe02ebff7e7bcce03ac67e70a66
roepckie-source <roepckie@gmail.com>
2026-07-21 16:50:39 +0200

    Update telegram_bot.py

--- outputs/telegram_bot.py
+++ outputs/telegram_bot.py
@@ -1,1 +1,69 @@
+import os
+import requests
+
+
+# ==============================
+# TELEGRAM CONFIG
+# ==============================
+
+TELEGRAM_TOKEN = os.environ.get(
+    "TELEGRAM_TOKEN"
+)
+
+TELEGRAM_CHAT_ID = os.environ.get(
+    "TELEGRAM_CHAT_ID"
+)
+
+
+# ==============================
+# SEND TELEGRAM MESSAGE
+# ==============================
+
+def send_telegram(message):
+
+    try:
+
+        url = (
+            f"https://api.telegram.org/"
+            f"bot{TELEGRAM_TOKEN}/sendMessage"
+        )
+
+
+        data = {
+
+            "chat_id": TELEGRAM_CHAT_ID,
+
+            "text": message,
+
+            "parse_mode": "HTML"
+
+        }
+
+
+        response = requests.post(
+            url,
+            data=data
+        )
+
+
+        if response.status_code == 200:
+
+            print(
+                "Telegram erfolgreich gesendet"
+            )
+
+        else:
+
+            print(
+                "Telegram Fehler:",
+                response.text
+            )
+
+
+    except Exception as e:
+
+        print(
+            "Telegram Fehler:",
+            e
+        )
 
