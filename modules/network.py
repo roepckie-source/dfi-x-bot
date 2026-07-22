da0c3353a5a992aab2f0ea499f8bf9bd712e4470
roepckie-source <roepckie@gmail.com>
2026-07-22 16:49:30 +0200

    Update network.py

--- modules/network.py
+++ modules/network.py
@@ -1,1 +1,62 @@
+# ======================================
+# DeFiChain Intelligence v4
+# Network Module
+# ======================================
 
+
+from modules.stats import get_stats
+
+
+
+
+def get_network_data():
+
+
+    stats = get_stats()
+
+
+    network = stats.get(
+        "network",
+        {}
+    )
+
+
+
+    return {
+
+
+        "network_status":
+
+        network.get(
+            "status",
+            "N/A"
+        ),
+
+
+
+        "block_height":
+
+        network.get(
+            "block_height",
+            "N/A"
+        ),
+
+
+
+        "last_block_time":
+
+        network.get(
+            "last_block_time",
+            "N/A"
+        ),
+
+
+
+        "masternodes":
+
+        network.get(
+            "masternodes",
+            "N/A"
+        )
+
+    }
