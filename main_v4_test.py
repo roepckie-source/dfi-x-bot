09f8c3b6abfbc12778cc59b70ed17eb17047e3cb
roepckie-source <roepckie@gmail.com>
2026-07-23 10:25:17 +0200

    Update main_v4_test.py

--- main_v4_test.py
+++ main_v4_test.py
@@ -12,7 +12,7 @@ from modules.blockchain import get_blockchain_data
 
 from modules.report_formatter import create_report
 from modules.intelligence import calculate_intelligence_score
-from modules.insights import generate_daily_insight
+from insights import generate_daily_insight
 from modules.history_engine import get_history
 
 
