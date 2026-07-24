0bfd70e9c5f099c452a38c9ce32b4bc89a5ae90f
roepckie-source <roepckie@gmail.com>
2026-07-24 13:33:00 +0200

    Update language.py

--- modules/language.py
+++ modules/language.py
@@ -1,1 +1,35 @@
+# ======================================
+# DeFiChain Intelligence v5
+# Language Loader
+# ======================================
 
+import json
+import os
+
+
+def load_language(language="de"):
+
+    file_path = os.path.join(
+        "languages",
+        f"{language}.json"
+    )
+
+    try:
+
+        with open(
+            file_path,
+            "r",
+            encoding="utf-8"
+        ) as file:
+
+            return json.load(file)
+
+
+    except Exception as e:
+
+        print(
+            "Language Load Fehler:",
+            e
+        )
+
+        return {}
