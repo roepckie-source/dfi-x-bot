6cab39ea9974a078c853edcdb41965b7f5fe4808
roepckie-source <roepckie@gmail.com>
2026-08-07 12:24:08 +0200

    Update main_v4_test.py

--- main_v4_test.py
+++ main_v4_test.py
@@ -10,6 +10,7 @@ from modules.dusd import get_dusd_data
 from modules.community import get_community_data
 from modules.blockchain import get_blockchain_data
 from modules.global_crypto import get_global_crypto
+from modules.language import load_language
 
 from modules.report_formatter import create_report
 from modules.intelligence import calculate_intelligence_score
